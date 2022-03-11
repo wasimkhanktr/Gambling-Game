733e83b7750589e9671a47da081f5263a4ccae29
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-11 09:20:23 +0530

    Gambling game simulator

---
+++ UC-1 Gamlbling/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_1_Gamlbling
+{
+    class Class1
+    {
+        public int stake = 100;
+        public int bet = 1;
+    }
+}
